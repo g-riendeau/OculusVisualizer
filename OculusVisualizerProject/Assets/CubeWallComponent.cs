8ce2260d19411f9032d573af503753268bd6fc36
g-riendeau <guillaume.riendeau@gmail.com>
2015-02-14 14:45:01 -0500

    [TASK] Created C# project [TASK] Create a script to generate a wall of cubes and another one to animate the cubes depending on their position (press play to see it in action!)

---
+++ OculusVisualizerProject/Assets/CubeWallComponent.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CubeWallComponent : MonoBehaviour 
+{
+	private const float kWidth = 10f;
+	private const float kDepth = 100f;
+
+	// Use this for initialization
+	void Start() 
+	{
+		for (float i = -(kWidth * 0.5f); i < (kWidth * 0.5f); i++)
+		{
+			for (float j = 0f; j < kDepth; j++)
+			{
+				GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+				cube.transform.parent = this.transform;
+				cube.transform.localPosition = new Vector3(i, 0f, j);
+				cube.transform.localScale = new Vector3(1f, 0.1f, 1f);
+
+				TestScaleEffectComponent effectComp = cube.AddComponent<TestScaleEffectComponent>();
+				effectComp.Initialize(i, j);
+			}
+		}
+	}
+}
---
+++ OculusVisualizerProject/Assets/TestScaleEffectComponent.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestScaleEffectComponent : MonoBehaviour 
+{
+	private float _x;
+	private float _y;
+
+	public void Initialize(float x, float y)
+	{
+		_x = x;
+		_y = y;
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		float scaleEffect = Mathf.Sin(_x * Time.time * 0.5f) * Mathf.Sin(_y * Time.time);
+		this.transform.localScale = new Vector3(1f, scaleEffect, 1f);
+	}
+}
