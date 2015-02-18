2f16427ee11b375b5d4c4b32dbb47e60013e128f
Lamalanimal <simon.racine@hotmail.com>
2015-02-17 21:56:58 -0500

    j

--- OculusVisualizerProject/Assets/CubeWallComponent.cs
+++ OculusVisualizerProject/Assets/CubeWallComponent.cs
@@ -4,20 +4,23 @@ using System.Collections.Generic;
 
 public class CubeWallComponent : MonoBehaviour 
 {
-	private const float kWidth = 10f;
-	private const float kDepth = 100f;
+	public Material FloorMat;
+	private const float cWidth = 10f;
+	private const float cDepth = 50f;
 
 	// Use this for initialization
 	void Start() 
 	{
-		for (float i = -(kWidth * 0.5f); i < (kWidth * 0.5f); i++)
+		for (float i = -(cWidth * 0.5f); i < (cWidth * 0.5f); i++)
 		{
-			for (float j = 0f; j < kDepth; j++)
+			for (float j = 0f; j < cDepth; j++)
 			{
 				GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
 				cube.transform.parent = this.transform;
 				cube.transform.localPosition = new Vector3(i, 0f, j);
+				cube.transform.localRotation = Quaternion.identity;
 				cube.transform.localScale = new Vector3(1f, 0.1f, 1f);
+				cube.renderer.material = FloorMat;
 
 				TestScaleEffectComponent effectComp = cube.AddComponent<TestScaleEffectComponent>();
 				effectComp.Initialize(i, j);
