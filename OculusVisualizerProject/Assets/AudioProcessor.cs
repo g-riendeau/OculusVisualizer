0ec053ea75d8a30b2eaaed764a9466f574b005bc
Felix G. Harvey <c212.felixh@gmail.com>
2015-02-17 20:32:51 -0500

    [TASK] Cleaned AudioProcessor

--- OculusVisualizerProject/Assets/AudioProcessor.cs
+++ OculusVisualizerProject/Assets/AudioProcessor.cs
@@ -13,12 +13,9 @@ public class AudioProcessor : MonoBehaviour {
 		float[] spectrum = audio.GetSpectrumData(4096, 0, FFTWindow.BlackmanHarris);
 		int i = 1;
 		while (i < 1023) {
-			//Debug.DrawLine(new Vector3(i - 1, spectrum[i] + 10, 0), new Vector3(i, spectrum[i + 1] + 10, 0), Color.red);
-			//Debug.DrawLine(new Vector3(i - 1, Mathf.Log(spectrum[i - 1]) + 10, 2), new Vector3(i, Mathf.Log(spectrum[i]) + 10, 2), Color.cyan);
 			Debug.DrawLine(new Vector3((float)((i-1)/100.0f - 5.12f), (float)( 12.0f+5*spectrum[i - 1]  - 10.0f ), 1.0f), 
 			               new Vector3((float)((i)/100.0f - 5.12f), (float)(12.0f+5*spectrum[i] - 10.0f), 1.0f), 
 			               Color.green);
-			//Debug.DrawLine(new Vector3(Mathf.Log(i - 1), Mathf.Log(spectrum[i - 1]), 3), new Vector3(Mathf.Log(i), Mathf.Log(spectrum[i]), 3), Color.yellow);
 			i++;
 		}
 	}
