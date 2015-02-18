0da1f57e994c9fa3e82ae9e04367c1ad33eac970
Lamalanimal <simon.racine@hotmail.com>
2015-02-17 23:07:57 -0500

    test

--- OculusVisualizerProject/Assets/TestScaleEffectComponent.cs
+++ OculusVisualizerProject/Assets/TestScaleEffectComponent.cs
@@ -1,11 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
+using System.Linq;
+
 
 public class TestScaleEffectComponent : MonoBehaviour 
 {
 	private float _x;
 	private float _y;
-
+	private float[] trees;
 	public void Initialize(float x, float y)
 	{
 		_x = x;
@@ -15,7 +18,9 @@ public class TestScaleEffectComponent : MonoBehaviour
 	// Update is called once per frame
 	void Update () 
 	{
+		trees = new float[2];
 		float scaleEffect = Mathf.Sin((2.0f*_x+0.5f*_y)*Time.time * 1.0f);
 		this.transform.localScale = new Vector3(1f, scaleEffect, 1f);
+
 	}
 }
