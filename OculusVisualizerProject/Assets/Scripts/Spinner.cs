7636956543d3be11f647670385c8427ef7cee2e5
Felix G. Harvey <c212.felixh@gmail.com>
2015-02-23 08:51:38 -0500

    Ajout d'un spinner qui marche pas. Lumière back in the tunnel.

---
+++ OculusVisualizerProject/Assets/Scripts/Spinner.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spinner : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Time.realtimeSinceStartup > 10f){
+			transform.Rotate(transform.forward, 5*Time.deltaTime);
+		}
+	}
+}
