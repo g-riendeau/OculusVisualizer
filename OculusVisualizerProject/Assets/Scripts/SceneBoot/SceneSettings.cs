ec79cba243418a316e2478f73552ad647f1cb239
griendeau <guillaume.riendeau@gmail.com>
2015-02-24 22:27:41 -0500

    Added transition doctor..

---
+++ OculusVisualizerProject/Assets/Scripts/SceneBoot/SceneSettings.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+[Serializable]
+public class SceneSettings 
+{
+	/// Scenes available
+	public enum MindfuckScenes
+	{
+		Cubes = 0,
+		Clouds = 1,
+		Creepy = 2
+	}
+	
+	[SerializeField]
+	private float _openingTransitionDuration;
+	[SerializeField]
+	private float _sceneDuration;
+	[SerializeField]
+	private float _closingTransitionDuration;
+	[SerializeField]
+	private MindfuckScenes _nextMindfuckScene;
+
+	/// <summary>
+	/// Gets the duration of the opening transition.
+	/// </summary>
+	public float OpeningTransitionDuration
+	{
+		get { return _openingTransitionDuration; }
+	}
+
+	/// <summary>
+	/// Gets the scene time.
+	/// </summary>
+	public float SceneDuration
+	{
+		get { return _sceneDuration; }
+	}
+
+	/// <summary>
+	/// Gets the duration of the closing transition.
+	/// </summary>
+	public float ClosingTransitionDuration
+	{
+		get { return _closingTransitionDuration; }
+	}
+
+	/// <summary>
+	/// Gets the name of the next scene.
+	/// </summary>
+	public string GetNextSceneName()
+	{
+		switch (_nextMindfuckScene)
+		{
+			case MindfuckScenes.Cubes:
+			{
+				return "scn_Cubes";
+			}
+			case MindfuckScenes.Clouds:
+			{
+				return "scn_Clouds";
+			}
+			case MindfuckScenes.Creepy:
+			{
+				return "scnbyoto_CreepyHouse";
+			}
+			default:
+			{
+				Debug.LogError("SceneSettings: Unknown scene");
+				return string.Empty;
+			}
+		}
+	}
+}
---
+++ OculusVisualizerProject/Assets/Scripts/SceneBoot/TransitionDoctor.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class TransitionDoctor : MonoBehaviour 
+{
+	public event Action<float> OnOpeningTransitionStarted = delegate {};
+	public event Action OnSceneStarted = delegate {};
+	public event Action<float> OnClosingTransitionStarted = delegate {};
+
+	[SerializeField]
+	private SceneSettings _sceneSettings;
+
+	private float _elapsedTime;
+
+	// Use this for initialization
+	void Start () 
+	{
+		if (_sceneSettings != null)
+		{
+			StartCoroutine(WaitForEndOfScene());
+		}
+		else
+		{
+			Debug.LogError("SceneStarter: Scene settings object was not found...");
+		}
+	}
+	
+	// Increment the elapsed time
+	void Update () 
+	{
+		_elapsedTime += Time.deltaTime;
+	}
+
+	/// <summary>
+	/// Waits till the time is elapsed
+	/// </summary>
+	private IEnumerator WaitForEndOfScene()
+	{
+		// Trigger the opening animation and gives the transition duration
+		OnOpeningTransitionStarted(_sceneSettings.OpeningTransitionDuration);
+
+		// Wait...
+		while (_elapsedTime < _sceneSettings.OpeningTransitionDuration)
+		{
+			yield return null;
+		}
+
+		// The opening transition is over, start animation stuff...
+		OnSceneStarted();
+		
+		// Wait...
+		_elapsedTime = 0;
+		while (_elapsedTime < _sceneSettings.SceneDuration)
+		{
+			yield return null;
+		}
+		
+		// Trigger the closing animation and gives the transition duration
+		OnClosingTransitionStarted(_sceneSettings.ClosingTransitionDuration);
+		
+		// Wait...
+		_elapsedTime = 0;
+		while (_elapsedTime < _sceneSettings.ClosingTransitionDuration)
+		{
+			yield return null;
+		}
+
+		// Load the level
+		Application.LoadLevel(_sceneSettings.GetNextSceneName());
+	}
+}
