013b9d26b74b3007887a2399de52b930cfa318c5
Lamalanimal <simon.racine@hotmail.com>
2015-02-28 12:59:30 -0500

    Changement au script de ghyslain et a la scene creepy (qui sera remplacé par Max donc a deleter)

---
+++ OculusVisualizerProject/Assets/Scripts/Creepy/gSphere.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gSphere 
+{
+
+	public GameObject go;
+	public float minDist;
+	public float maxDist;	
+	
+	public gSphere(GameObject newgo, float newminDist,float newmaxDist)
+	{			
+		go = newgo;
+		minDist = newminDist;
+		maxDist = newmaxDist;
+
+	}
+
+}
