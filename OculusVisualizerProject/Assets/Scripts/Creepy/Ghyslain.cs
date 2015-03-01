c44757c05ab363c3d60f2d1308eabcd40e8f1fcd
Lamalanimal <simon.racine@hotmail.com>
2015-03-01 17:24:54 -0500

    Changements Scènes creepy. On trigger event ne semble pas fonctionner. À vérifier

--- OculusVisualizerProject/Assets/Scripts/Creepy/Ghyslain.cs
+++ OculusVisualizerProject/Assets/Scripts/Creepy/Ghyslain.cs
@@ -5,7 +5,10 @@ using System.Collections.Generic;
 public class Ghyslain : MonoBehaviour {
 
 	public List<gSphere> sphereList;
-
+	private List<GameObject> sphereInRangeList;		//All spheres that are in range but not pulled in Ghyslain gravity
+	private float timerOnAddSphere = 2000;
+	private float timer = 0;
+	private int pulledSphere;
 	public Material sphereMat;
 	public GameObject Eclair;
 	public GameObject evilLight;
@@ -18,7 +21,7 @@ public class Ghyslain : MonoBehaviour {
 	private float iniSpherePos;
 
 	// parametres qui influencent la force
-	private float attractionForce = 300f;
+	private float attractionForce;
 	private float topSpeed = 10f;
 	private float cDrag = 0.5f;
 
@@ -27,7 +30,7 @@ public class Ghyslain : MonoBehaviour {
 
 		//Creating the listof spheres
 		sphereList = new List<gSphere>();
-		//sphereList = new List<GameObject>();
+		sphereInRangeList = new List<GameObject>();
 
 		attractionForce = 2000;
 		sphereNb = 13;
@@ -44,19 +47,20 @@ public class Ghyslain : MonoBehaviour {
 			iniSpherePos = Random.Range(10,20);
 
 			//La position de la sphere est variante en x seulement (plus facile pour savoir quelle vitesse lui appliquer pour qu'elle entre en orbite
-			uneSphere.transform.localPosition = new Vector3(transform.position.x+iniSpherePos, transform.position.y+i, transform.position.z);
+			uneSphere.transform.position = new Vector3(transform.position.x+iniSpherePos, transform.position.y+i, transform.position.z);
 			uneSphere.transform.localRotation = Quaternion.identity;
 			uneSphere.transform.localScale = new Vector3(1f, 1f, 1f);
 			uneSphere.AddComponent<Rigidbody>();
 			uneSphere.rigidbody.angularDrag = 0;
 			uneSphere.rigidbody.mass = 1;
+			uneSphere.gameObject.tag = "ActiveSphere";
 
 			// On donne a la sphere une vitesse en z avec direction aléatoirement positive ou négative
 			if (Random.Range(-1,1) < 0){
-				uneSphere.rigidbody.velocity = new Vector3(0,0,400/uneSphere.transform.position.x);
+				uneSphere.rigidbody.velocity = new Vector3(0,0,400/iniSpherePos);
 			}
 			else{
-				uneSphere.rigidbody.velocity = new Vector3(0,0,-400/uneSphere.transform.position.x);
+				uneSphere.rigidbody.velocity = new Vector3(0,0,-400/iniSpherePos);
 			}
 
 			sphereList.Add (new gSphere(uneSphere,Random.Range (3,8),Random.Range (22,35)));
@@ -75,6 +79,15 @@ public class Ghyslain : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
+		// timerOnAddSphere varies with the number of spheres in the list sphereInRangeList
+		timer += Time.deltaTime;
+		if (timer > timerOnAddSphere)	{
+			timer=0;
+			pulledSphere = Random.Range( 0, sphereInRangeList.Count );
+			PullSphere(sphereInRangeList[pulledSphere]);
+			sphereInRangeList.RemoveAt(pulledSphere);
+		}
+
 		// Mouvement simple de Ghyslain pour tester le ontriggerCollider
 		transform.position = new Vector3 (transform.position.x+0.01f, transform.position.y, transform.position.z);
 
@@ -83,21 +96,15 @@ public class Ghyslain : MonoBehaviour {
 			//Calcul de la distance relative de la sphere avec Ghyslain
 			relDist = transform.position - sphereList [i].go.transform.position;
 
+			/*
 			// kind of drag en v^4 mais pas dans la direction de la vitesse
 			if( sphereList[i].go.rigidbody.velocity.magnitude > topSpeed ){
 				sphereList[i].go.rigidbody.AddForce ( relDist / relDist.magnitude *
 				                                     cDrag * Mathf.Pow (sphereList[i].go.rigidbody.velocity.magnitude/topSpeed , 4f) );
 			}
-
-
-			if (relDist.magnitude > 200){
-				Destroy(sphereList[i].go);
-				sphereList.Remove(sphereList[i]);
-				Debug.Log ("Sphere destroyed cause it was out of range");
-
-			}
+*/
 			// force si la sphere s'éloigne trop
-			else if (relDist.magnitude > sphereList[i].maxDist){
+			if (relDist.magnitude > sphereList[i].maxDist){
 				sphereList[i].go.rigidbody.AddForce ((50*(relDist.magnitude-sphereList[i].maxDist)/relDist.magnitude+1)*attractionForce / Mathf.Pow (relDist.magnitude, 3f) * relDist);
 
 			}
@@ -112,6 +119,7 @@ public class Ghyslain : MonoBehaviour {
 			// anti gravite
 			sphereList[i].go.rigidbody.AddForce (0, Mathf.Max (10, 100/( Mathf.Pow(relDist.magnitude,1)+0.1f)),0);
 
+
 			/*
 			// force en 1/r^2
 			spheres[i].rigidbody.AddForce (attractionForce / Mathf.Pow (relDist.magnitude, 3f) * relDist);
@@ -121,10 +129,16 @@ public class Ghyslain : MonoBehaviour {
 			spheres[i].rigidbody.AddForce (0, 10, 0);
 
 			eclairs[i].SetPosition (0, transform.position);
-			eclairs[i].SetPosition (1, spheres[i].transform.position);
-*/
-		}
+			eclairs[i].SetPosition (1, spheres[i].transform.position);*/
 
+			
+			if (relDist.magnitude > 200){
+				Destroy(sphereList[i].go);
+				sphereList.Remove(sphereList[i]);
+				Debug.Log ("Sphere destroyed cause it was out of range");
+				
+			}
+		}
 		
 		// Un éclair!
 		if (Time.realtimeSinceStartup > temps1)	{
@@ -146,6 +160,7 @@ public class Ghyslain : MonoBehaviour {
 			temps1 = temps1+Random.Range (2,15);			
 			evilLight.light.intensity = Mathf.Lerp (evilLight.light.intensity,1,Time.deltaTime*50);
 			evilLight.light.range =  Mathf.Lerp (evilLight.light.range,20,Time.deltaTime*50);
+
 		}	
 	}
 
@@ -155,9 +170,20 @@ public class Ghyslain : MonoBehaviour {
 
 	}
 
-	public void AddThisSphere(GameObject uneSphere)  {
+	// Add a new sphere to Gyslain gravitational pull
+	public void PullSphere(GameObject uneSphere)  {
 		uneSphere.transform.rigidbody.isKinematic = false;
 		sphereList.Add (new gSphere(uneSphere,Random.Range (3,8),Random.Range (22,35)));
+
+	}
+
+	// Add a new sphere. A soft pull will attrack some spheres in range
+	public void SphereInRange(GameObject uneSphere)  {
+		Debug.Log ("salut");
+		sphereInRangeList.Add (uneSphere);
+		timerOnAddSphere = 20 / sphereInRangeList.Count;
+		Debug.Log (sphereInRangeList.Count);
+		//Debug.Log (timerOnAddSphere);
 
 	}
 
--- OculusVisualizerProject/Assets/Scripts/Creepy/SphereAttraction.cs
+++ OculusVisualizerProject/Assets/Scripts/Creepy/SphereAttraction.cs
@@ -3,16 +3,16 @@ using System.Collections;
 
 public class SphereAttraction : MonoBehaviour {
 
+
 	private GameObject Enterer;
 	
 	void OnTriggerEnter(Collider Enterer) {
-
 		if (Enterer.tag == "PassiveSphere") {
-			Debug.Log (Enterer);
 
-			GetComponent<Ghyslain> ().AddThisSphere(Enterer.gameObject);
+				//GetComponentInParent<Ghyslain> ().PullSphere(Enterer.gameObject);
+			GetComponentInParent<Ghyslain> ().SphereInRange(Enterer.gameObject);
+			Enterer.tag = "ActiveSphere";
 		}
-
 
 	}
 }
