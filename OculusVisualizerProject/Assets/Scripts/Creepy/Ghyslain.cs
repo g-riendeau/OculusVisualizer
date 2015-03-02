d16a3dfaed6f3bdcd3ee916f3d70adadc5f02118
Maxime Tousignant <maxtou@hotmail.com>
2015-03-02 17:05:56 -0500

    Ghyslain sans bug, mais work in progress

--- OculusVisualizerProject/Assets/Scripts/Creepy/Ghyslain.cs
+++ OculusVisualizerProject/Assets/Scripts/Creepy/Ghyslain.cs
@@ -4,48 +4,44 @@ using System.Collections.Generic;
 
 public class Ghyslain : MonoBehaviour {
 
-	public List<gSphere> sphereList;
-	private List<GameObject> sphereInRangeList;		//All spheres that are in range but not pulled in Ghyslain gravity
-	private float timerOnAddSphere = 2000;
-	private float timer = 0;
-	private int pulledSphere;
+	public List<gSphere> sphereList = new List<gSphere>();
 	public Material sphereMat;
 	public GameObject Eclair;
 	public GameObject evilLight;
 
-	private int sphereNb;
-	private GameObject uneSphere;
-    private LineRenderer[] eclairs;
-	private Vector3 relDist;
-	private float temps1 = 5;
-	private float iniSpherePos;
+	private List<GameObject> sphereInRangeList  = new List<GameObject>();		//All spheres that are in range but not pulled in Ghyslain gravity
+	private float timerOnAddSphere = 2000;
+	private float timer = 0;
+	private int pulledSphere;
 
+	// pour l'initialisation
+	private GameObject uneSphere;
+	private int iniSphereNb = 13;
+	private float iniSpherePos;
+	private float iniExcentricite;
+	private float iniAttractionForceXZ;
+
+	// pour l'elimination
+	private float criticalDist = 200f;
+//	private List<int> sphereToDeleteList = new List<int>();
+	
 	// parametres qui influencent la force
-	private float attractionForce;
+	private Vector3 relDist;
+	private float forceChangeRate = 500f;
 	private float topSpeed = 10f;
 	private float cDrag = 0.5f;
 
+	private float temps1 = 5;
+	
 	// Use this for initialization
 	void Start () {
 
-		//Creating the listof spheres
-		sphereList = new List<gSphere>();
-		sphereInRangeList = new List<GameObject>();
-
-		attractionForce = 2000;
-		sphereNb = 13;
-
-		// On créé une matrices qui contient tous les objects spheres qui orbitent autour de Ghyslain
-		//Spheres = new GameObject[sphereNb];
-		eclairs = new LineRenderer[sphereNb];
-
-		for (int i=0; i<sphereNb; i++) {
-
+		for (int i=0; i<iniSphereNb; i++) {
 			//Création d'une sphere
 			uneSphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
 			uneSphere.renderer.material = sphereMat;
-			iniSpherePos = Random.Range(10,20);
-
+			iniSpherePos = Random.Range(10f,20f);
+			
 			//La position de la sphere est variante en x seulement (plus facile pour savoir quelle vitesse lui appliquer pour qu'elle entre en orbite
 			uneSphere.transform.position = new Vector3(transform.position.x+iniSpherePos, transform.position.y+i, transform.position.z);
 			uneSphere.transform.localRotation = Quaternion.identity;
@@ -54,91 +50,53 @@ public class Ghyslain : MonoBehaviour {
 			uneSphere.rigidbody.angularDrag = 0;
 			uneSphere.rigidbody.mass = 1;
 			uneSphere.gameObject.tag = "ActiveSphere";
-
+			
 			// On donne a la sphere une vitesse en z avec direction aléatoirement positive ou négative
-			if (Random.Range(-1,1) < 0){
-				uneSphere.rigidbody.velocity = new Vector3(0,0,400/iniSpherePos);
-			}
-			else{
-				uneSphere.rigidbody.velocity = new Vector3(0,0,-400/iniSpherePos);
-			}
-
-			sphereList.Add (new gSphere(uneSphere,Random.Range (3,8),Random.Range (22,35)));
-		
-			/* Création d'un éclair entre chaque sphere p/r a Ghyslain
-			GameObject unEclair = new GameObject();
-			unEclair.AddComponent<LineRenderer>();
-			eclairs[i] = unEclair.GetComponent<LineRenderer>();
-			eclairs[i].SetPosition( 0, transform.position );
-			eclairs[i].SetPosition( 1, spheres[i].transform.position );
-			eclairs[i].SetWidth( 0.1f, 0.1f);
-			*/
+			iniExcentricite = Random.Range(0,1.5f);
+			iniAttractionForceXZ = Random.Range(1500f,2500f);
+			uneSphere.rigidbody.velocity = new Vector3(0,0, (0.5f+iniExcentricite) * Mathf.Sqrt(iniAttractionForceXZ/iniSpherePos));
+			sphereList.Add ( new gSphere(uneSphere, Random.Range(3f,8f), Random.Range(22f,35f), iniAttractionForceXZ ) );
 		}
 	}
 
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
 
 		// timerOnAddSphere varies with the number of spheres in the list sphereInRangeList
 		timer += Time.deltaTime;
 		if (timer > timerOnAddSphere)	{
-			timer=0;
+			timer = 0;
 			pulledSphere = Random.Range( 0, sphereInRangeList.Count );
 			PullSphere(sphereInRangeList[pulledSphere]);
 			sphereInRangeList.RemoveAt(pulledSphere);
 		}
 
 		// Mouvement simple de Ghyslain pour tester le ontriggerCollider
-		transform.position = new Vector3 (transform.position.x+0.01f, transform.position.y, transform.position.z);
+		transform.position = new Vector3 (transform.position.x + 1f*Time.deltaTime, transform.position.y, transform.position.z);
+		
+		
+		for (int i = sphereList.Count-1; i >= 0; i--) {
 
-
-		for (int i = sphereList.Count - 1; i >= 0; i--) {
 			//Calcul de la distance relative de la sphere avec Ghyslain
 			relDist = transform.position - sphereList [i].go.transform.position;
 
-			/*
-			// kind of drag en v^4 mais pas dans la direction de la vitesse
-			if( sphereList[i].go.rigidbody.velocity.magnitude > topSpeed ){
-				sphereList[i].go.rigidbody.AddForce ( relDist / relDist.magnitude *
-				                                     cDrag * Mathf.Pow (sphereList[i].go.rigidbody.velocity.magnitude/topSpeed , 4f) );
+
+			if (relDist.magnitude > criticalDist ){
+			Destroy( sphereList[i].go );
+			sphereList.Remove( sphereList[i] );
+			Debug.Log ("Sphere destroyed cause it was out of range");
 			}
-*/
-			// force si la sphere s'éloigne trop
-			if (relDist.magnitude > sphereList[i].maxDist){
-				sphereList[i].go.rigidbody.AddForce ((50*(relDist.magnitude-sphereList[i].maxDist)/relDist.magnitude+1)*attractionForce / Mathf.Pow (relDist.magnitude, 3f) * relDist);
-
-			}
-			// force d'attraction normal pour un corp en orbite
-			else if (relDist.magnitude > sphereList[i].minDist){
-				sphereList[i].go.rigidbody.AddForce (attractionForce / Mathf.Pow (relDist.magnitude, 3f) * relDist);
-			}
-			//La sphere est trop proche. Donner une force négligeable pour ne pas qu'elle reste coincé au milieu
-			else {
-				sphereList[i].go.rigidbody.AddForce (Vector3.Cross(new Vector3(0,10,0),relDist));	
-			}
-			// anti gravite
-			sphereList[i].go.rigidbody.AddForce (0, Mathf.Max (10, 100/( Mathf.Pow(relDist.magnitude,1)+0.1f)),0);
-
-
-			/*
-			// force en 1/r^2
-			spheres[i].rigidbody.AddForce (attractionForce / Mathf.Pow (relDist.magnitude, 3f) * relDist);
-			// force de repulsion a courte distance en 1/r^5
-			spheres[i].rigidbody.AddForce (- attractionForce / Mathf.Pow (relDist.magnitude, 6f) * relDist);
-			// anti gravite
-			spheres[i].rigidbody.AddForce (0, 10, 0);
-
-			eclairs[i].SetPosition (0, transform.position);
-			eclairs[i].SetPosition (1, spheres[i].transform.position);*/
-
 			
-			if (relDist.magnitude > 200){
-				Destroy(sphereList[i].go);
-				sphereList.Remove(sphereList[i]);
-				Debug.Log ("Sphere destroyed cause it was out of range");
-				
-			}
+			// forece d'attraction
+			AddForceXZ ( relDist, sphereList[i] );
+			//AddDrag( sphereMat[i] );
+			
+			
+			// anti gravite
+			//sphereList[i].go.rigidbody.AddForce (0, Mathf.Max (9, 100/(relDist.y + 0.5f) ),0);
+			sphereList[i].go.rigidbody.AddForce (0, 9.81f,0);
 		}
+
 		
 		// Un éclair!
 		if (Time.realtimeSinceStartup > temps1)	{
@@ -167,13 +125,43 @@ public class Ghyslain : MonoBehaviour {
 	IEnumerator WaitAndDestroy(float waitTime,GameObject gameObject) {
 		yield return new WaitForSeconds(waitTime);
 		Destroy(gameObject);
-
 	}
+
+	// calcul de la force radiale
+	private void AddForceXZ( Vector3 relDist, gSphere sphere){
+		Vector2 relDistXZ = new Vector2 (relDist.x, relDist.z);
+		Vector2 forceXZ = new Vector2(0f,0f);
+		
+		if (relDistXZ.magnitude < sphere.minDist) {
+			sphere.attractionForceXZ -= forceChangeRate * Time.deltaTime;
+			if (sphere.attractionForceXZ < 0f)
+				sphere.attractionForceXZ = 0f;
+		}
+		else if (relDistXZ.magnitude > sphere.maxDist) {
+			sphere.attractionForceXZ += forceChangeRate * Time.deltaTime;
+		}
+		forceXZ = sphere.attractionForceXZ / Mathf.Pow (relDistXZ.magnitude, 3f) * relDistXZ;
+		sphere.go.rigidbody.AddForce( new Vector3(forceXZ.x,0f,forceXZ.y) );
+	}
+
+	private void AddDrag( gSphere sphere){
+	
+	}
+
+//	// routine pour supprimer les pheres perdues
+//	private void DeleteSpheres(  List<gSphere> sphereList, List<int> sphereToDeleteList ){
+//		for (int i=0; i<sphereToDeleteList.Count; i++) {
+//			Destroy( sphereList[sphereToDeleteList[i]].go );
+//			sphereList.Remove( sphereList[sphereToDeleteList[i]] );
+//			Debug.Log ("Sphere destroyed cause it was out of range");
+//		}
+//		sphereToDeleteList = new List<int> ();
+//	}
 
 	// Add a new sphere to Gyslain gravitational pull
 	public void PullSphere(GameObject uneSphere)  {
 		uneSphere.transform.rigidbody.isKinematic = false;
-		sphereList.Add (new gSphere(uneSphere,Random.Range (3,8),Random.Range (22,35)));
+		sphereList.Add (new gSphere(uneSphere,Random.Range (3,8),Random.Range (22,35), Random.Range (1500f,2500f)));
 		sphereInRangeList.RemoveAt(pulledSphere);
 	}
 
@@ -181,7 +169,6 @@ public class Ghyslain : MonoBehaviour {
 	public void SphereInRange(GameObject uneSphere)  {
 		sphereInRangeList.Add (uneSphere);
 		timerOnAddSphere = Mathf.Clamp(1,50 / Mathf.Pow (sphereInRangeList.Count,0.5f),20);
-		Debug.Log (timerOnAddSphere);
 		//Debug.Log (timerOnAddSphere);
 
 	}
