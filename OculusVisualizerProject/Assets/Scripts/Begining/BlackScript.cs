b3eb137d41b4db534650da3dc8a0297d42c215ed
Felix G. Harvey <c212.felixh@gmail.com>
2015-04-07 16:47:57 -0400

    Code pour l'ajout continue de lignes noires.

--- OculusVisualizerProject/Assets/Scripts/Begining/BlackScript.cs
+++ OculusVisualizerProject/Assets/Scripts/Begining/BlackScript.cs
@@ -1,72 +1,109 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class BlackScript : MonoBehaviour {
 
-	public Material BlackToonMat;
-	private LineRenderer blackLine; 
+	public Material blackToonMat;
+	private List<LineRenderer> lines; 
 	private float tailSpeed;
 	private float headSpeed;
 	private float zSpeed;
 	private float zStart;
-	private Vector3 headTo;
-	private Vector3 p0; //Line positions
-	private Vector3 p1;
-	private  float spikeTimeInterval; // secondes
-	private  float timeSinceLastSpike; // secondes
+	private List<Vector3> headTo; //head objective
+	private List<Vector3> p0s; 	//head positions
+	private List<Vector3> p1s;	// tail positions
+	private List<float> spikeTimeIntervals; // secondes
+	private  float lineTimeInterval; // secondes
+	private  List<float> timesSinceLastSpike; // secondes
+	private  float timeSinceLastLine; // secondes
+	private  float planeWidth;
 
 	// Use this for initialization
 	void Start () {
-	
-		//Initialize line positions
-		blackLine = GetComponent<LineRenderer>();
+
+		//Initiliazations
+		lines = new List<LineRenderer>(); 
+		p0s = new List<Vector3>();
+		p1s = new List<Vector3>();
+		headTo = new List<Vector3>();
+		timesSinceLastSpike = new List<float>();
+		spikeTimeIntervals = new List<float>();
 		zStart = 100f;
-		p0 = new Vector3(0f,0f,zStart);
-		p1 = new Vector3(0.5f,0f,zStart);
-		headTo = new Vector3(0f, 0f, zStart);
-		blackLine.SetPosition(0, p0);
-		blackLine.SetPosition(1, p1);
-		blackLine.SetColors(Color.blue, Color.red);
-		zSpeed = 2f;
-		blackLine.material = new Material(Shader.Find("Particles/Alpha Blended"));
-		blackLine.SetColors(Color.black, Color.white);
-		tailSpeed = 8f;
-		headSpeed = 25f;
-		timeSinceLastSpike = 0f;
-		spikeTimeInterval = 0.1f;
+		zSpeed = 200;
+		tailSpeed = 5f;
+		headSpeed = 7f;
+		timeSinceLastLine = 0f;
+		lineTimeInterval = 5f;
+		planeWidth = 15f;
+
+		//Add first line
+		addLine();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		timeSinceLastSpike += Time.deltaTime;
-		if(timeSinceLastSpike > spikeTimeInterval)
-		{
-			//Vector3 spike = new Vector3(0f,0f,0f);
-			Vector3 spike = new Vector3(Random.Range (-7f,7f), Random.Range (-7f,7f), Mathf.Max (zStart - zSpeed*Time.time, 0f)); 	
-			headTo = spike;
-			timeSinceLastSpike = 0f;
-			spikeTimeInterval = Random.Range (0.05f,0.7f);
-
+		timeSinceLastLine += Time.deltaTime;
+		if (timeSinceLastLine > lineTimeInterval){
+			addLine ();
+			timeSinceLastLine = 0f;
 		}
 
-
-		blackLine.SetPosition(0, p0);
-		seek (Time.deltaTime, headTo);
-		follow (Time.deltaTime);
+		for (int i = 0 ; i < lines.Count ; i++)
+		{
+			findNewSpikes(i);
+			lines[i].SetPosition(0, p0s[i]);
+			seek (Time.deltaTime, i);
+			follow (Time.deltaTime, i);
+		}
 	}
 
-	void follow(float elaspedTime){
+	//Find new headTo
+	void findNewSpikes(int idx)
+	{
+		timesSinceLastSpike[idx] += Time.deltaTime;
+		if(timesSinceLastSpike[idx] > spikeTimeIntervals[idx])
+		{
+			//Vector3 spike = new Vector3(0f,0f,0f);
+			Vector3 spike = new Vector3(Random.Range (-planeWidth,planeWidth), Random.Range (-planeWidth,planeWidth), Mathf.Max (p0s[idx].z - zSpeed*Time.deltaTime, 0f)); 	
+			headTo[idx] = spike;
+			timesSinceLastSpike[idx] = 0f;
+			spikeTimeIntervals[idx] = Random.Range (0.1f,1.1f);
+		}
+	}
+
+	//Tail of a line following the head
+	void follow(float elaspedTime, int idx){
 	
-		Vector3 dir = p0 - p1;
-		p1 = p1 + (tailSpeed * elaspedTime *  dir);
-		blackLine.SetPosition(1, p1);
+		Vector3 dir = p0s[idx] - p1s[idx];
+		p1s[idx] = p1s[idx] + (tailSpeed * elaspedTime *  dir);
+		lines[idx].SetPosition(1, p1s[idx]);
 	}
 
-	void seek(float elaspedTime, Vector3 headTo){
+	//Head of line seeking headTo
+	void seek(float elaspedTime, int idx){
 		
-		Vector3 dir = headTo - p0;
-		p0 = p0 + (headSpeed * elaspedTime *  dir);
-		blackLine.SetPosition(0, p0);
+		Vector3 dir = headTo[idx] - p0s[idx];
+		p0s[idx] = p0s[idx] + (headSpeed * elaspedTime *  dir);
+		lines[idx].SetPosition(0, p0s[idx]);
+	}
+
+	//Add new line
+	void addLine(){
+		GameObject line = new GameObject();
+		LineRenderer blackLine = line.AddComponent<LineRenderer>();
+		lines.Add(blackLine);
+		p0s.Add (new Vector3(0f,0f,zStart));
+		p1s.Add (new Vector3(0.5f,0f,zStart));
+		headTo.Add(new Vector3(0f, 0f, zStart));
+		int max = lines.Count-1;
+		spikeTimeIntervals.Add (1.0f);
+		lines[max].SetPosition(0, p0s[0]);
+		lines[max].SetPosition(1, p1s[0]);
+		lines[max].SetWidth(0.1f, 0.07f);		
+		lines[max].material = new Material(Shader.Find("Diffuse"));
+		lines[max].SetColors(Color.black, Color.white);
+		timesSinceLastSpike.Add(0f);
 	}
 }
