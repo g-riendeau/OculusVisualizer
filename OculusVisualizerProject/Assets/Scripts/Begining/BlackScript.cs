d204d35d4e4338e1c4f116a2c6d23b678a6a7397
Felix G. Harvey <c212.felixh@gmail.com>
2015-04-14 21:34:12 -0400

    Deux plans dans le begining.

--- OculusVisualizerProject/Assets/Scripts/Begining/BlackScript.cs
+++ OculusVisualizerProject/Assets/Scripts/Begining/BlackScript.cs
@@ -10,15 +10,19 @@ public class BlackScript : MonoBehaviour {
 	private float headSpeed;
 	private float zSpeed;
 	private float zStart;
+	private float zStop;
 	private List<Vector3> headTo; //head objective
 	private List<Vector3> p0s; 	//head positions
 	private List<Vector3> p1s;	// tail positions
 	private List<float> spikeTimeIntervals; // secondes
-	private  float lineTimeInterval; // secondes
-	private  List<float> timesSinceLastSpike; // secondes
-	private  float timeSinceLastLine; // secondes
-	private  float planeWidth;
+	private float lineTimeInterval; // secondes
+	private List<float> timesSinceLastSpike; // secondes
+	private float timeSinceLastLine; // secondes
+	private float planeWidth;
 
+	float Rad(float deg){
+		return deg * (Mathf.PI/180.0f);
+	}
 	// Use this for initialization
 	void Start () {
 
@@ -29,13 +33,22 @@ public class BlackScript : MonoBehaviour {
 		headTo = new List<Vector3>();
 		timesSinceLastSpike = new List<float>();
 		spikeTimeIntervals = new List<float>();
-		zStart = 100f;
-		zSpeed = 200;
-		tailSpeed = 5f;
-		headSpeed = 7f;
+		zStart = Mathf.Cos (Rad(transform.parent.localRotation.eulerAngles.y)) *100.0f + (transform.parent.localPosition.z);
+
+		zStop = transform.parent.localPosition.z;
+		zSpeed = Mathf.Abs(5.0f*(zStart-zStop));
+
+		tailSpeed = 3f;
+		headSpeed = 5f;
 		timeSinceLastLine = 0f;
-		lineTimeInterval = 5f;
-		planeWidth = 15f;
+		lineTimeInterval = 0.5f;
+		planeWidth = 20;
+
+		Debug.Log ("zStart : " + zStart);
+		Debug.Log ("rotY : " + Mathf.Cos(Rad(transform.parent.localRotation.eulerAngles.y)));
+		Debug.Log ("zStop : " + zStop);
+		Debug.Log ("zSpeed : " + zSpeed);
+
 
 		//Add first line
 		addLine();
@@ -52,25 +65,30 @@ public class BlackScript : MonoBehaviour {
 
 		for (int i = 0 ; i < lines.Count ; i++)
 		{
-			findNewSpikes(i);
-			lines[i].SetPosition(0, p0s[i]);
+			if(findNewSpikes(i)){
+				lines[i].SetPosition(0, p0s[i]);
+			}
 			seek (Time.deltaTime, i);
 			follow (Time.deltaTime, i);
 		}
 	}
 
 	//Find new headTo
-	void findNewSpikes(int idx)
+	bool findNewSpikes(int idx)
 	{
 		timesSinceLastSpike[idx] += Time.deltaTime;
 		if(timesSinceLastSpike[idx] > spikeTimeIntervals[idx])
 		{
 			//Vector3 spike = new Vector3(0f,0f,0f);
-			Vector3 spike = new Vector3(Random.Range (-planeWidth,planeWidth), Random.Range (-planeWidth,planeWidth), Mathf.Max (p0s[idx].z - zSpeed*Time.deltaTime, 0f)); 	
+			Vector3 spike = new Vector3(Random.Range (-planeWidth,planeWidth), 
+			                            Random.Range (-planeWidth,planeWidth), 
+			                            Mathf.Max (p0s[idx].z - zSpeed*Time.deltaTime, zStop)); 	
 			headTo[idx] = spike;
 			timesSinceLastSpike[idx] = 0f;
-			spikeTimeIntervals[idx] = Random.Range (0.1f,1.1f);
+			spikeTimeIntervals[idx] = Random.Range (0.5f,2.0f);
+			return true;
 		}
+		return false;
 	}
 
 	//Tail of a line following the head
@@ -101,9 +119,9 @@ public class BlackScript : MonoBehaviour {
 		spikeTimeIntervals.Add (1.0f);
 		lines[max].SetPosition(0, p0s[0]);
 		lines[max].SetPosition(1, p1s[0]);
-		lines[max].SetWidth(0.1f, 0.07f);		
-		lines[max].material = new Material(Shader.Find("Diffuse"));
-		lines[max].SetColors(Color.black, Color.white);
+		lines[max].SetWidth(0.5f, 0.01f);		
+		lines[max].material = new Material(Shader.Find("Particles/Multiply"));
+		lines[max].SetColors(Color.blue, Color.green);
 		timesSinceLastSpike.Add(0f);
 	}
 }
